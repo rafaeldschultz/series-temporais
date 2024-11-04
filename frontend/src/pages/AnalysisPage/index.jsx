e7e32d01952f512006d34c172e67a5703b1ab956
ciro.falsarella <cirofalsarella@usp.br>
2024-11-04 18:00:25 -0300

    FIX: Removing breadcrumbs

--- frontend/src/pages/AnalysisPage/index.jsx
+++ frontend/src/pages/AnalysisPage/index.jsx
@@ -1,31 +1,13 @@
 import {
-  Autocomplete,
   Box,
-  Breadcrumbs,
-  Drawer,
-  Link,
-  Stack,
-  TextField,
-  Typography,
 } from "@mui/material";
 import Page from "../../layouts/Page";
 import SideMenu from "../../components/SideMenu/SideMenu";
-import NavigateNextRoundedIcon from "@mui/icons-material/NavigateNextRounded";
-import LinePlot from "../../components/Charts/LinePlot";
-import Grid from "@mui/material/Grid2";
-import TemporalSeriesChart from "./Sections/temporalSeriesChart";
 import { useState } from "react";
 import { navigationItems } from "./Sections/navigation";
 
 const AnalysisPage = () => {
   const [visibleChart, setVisibleChart] = useState(1);
-
-  const lastHeader = navigationItems
-    .slice(0, visibleChart) // Only consider items before the current selection
-    .reverse() // Reverse to find the last "header" by searching from the end
-    .find((item) => item.kind === "header");
-  const currentTitle = navigationItems[visibleChart]?.title || "";
-  const headerTitle = lastHeader ? lastHeader.title : "";
 
   return (
     <Page sx={{ p: 0 }}>
@@ -36,26 +18,6 @@ const AnalysisPage = () => {
           selectedItemId={visibleChart}
         />
         <Box sx={{ p: 4, width: "100%", height: "100%" }}>
-          <Breadcrumbs
-            aria-label="breadcrumb"
-            separator={
-              <NavigateNextRoundedIcon fontSize="small" color="primary" />
-            }
-          >
-            <Link color="primary" href="/analysis">
-              Análise Estatística
-            </Link>
-            {headerTitle !== "" && (
-              <Typography underline="hover" color="primary">
-                {headerTitle}
-              </Typography>
-            )}
-            {currentTitle !== "" && (
-              <Typography underline="hover" color="primary">
-                {currentTitle}
-              </Typography>
-            )}
-          </Breadcrumbs>
           {navigationItems[visibleChart].component}
         </Box>
       </Box>
