ebf6d9f2018b0e74cd8e54f3d30272e2889c9f34
ciro.falsarella <cirofalsarella@usp.br>
2024-11-23 19:08:18 -0300

    feat: preview (wrong data)

--- frontend/src/pages/HomePage/Preview/index.jsx
+++ frontend/src/pages/HomePage/Preview/index.jsx
@@ -1,12 +1,62 @@
-import { Typography } from "@mui/material";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+const columns = [
+  'Dessert', 'Calories', 'Fat (g)', 'Carbs (g)', 'Protein (g)'
+];
 
 const Preview = () => {
   return (
     <>
-      <Typography variant="h4" gutterBottom sx={{color: '#2969BD', padding:'20px'}}>
-        Preview
-      </Typography>
+      <TableContainer>
+        <Table sx={{ minWidth: 300, }} size="small" aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell>
+                  {column}
+                </TableCell>
+              ))
+              }
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.calories}</TableCell>
+                <TableCell align="center">{row.fat}</TableCell>
+                <TableCell align="center">{row.carbs}</TableCell>
+                <TableCell align="center">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
-  )
+  );
 }
+
 export default Preview;
