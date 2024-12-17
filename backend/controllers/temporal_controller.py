0e9b4e3292e95601b3a2c1daef15e202d313055d
sozrafael <89589108+Rafaelsoz@users.noreply.github.com>
2024-12-16 21:45:25 -0300

    Update Temporal Controller
    Adding new functions and new infos in controller

--- backend/controllers/temporal_controller.py
+++ backend/controllers/temporal_controller.py
@@ -4,10 +4,19 @@ from typing import Optional
 
 import numpy as np
 import pandas as pd
+
 from statsmodels.api import tsa
 from statsmodels.graphics.tsaplots import plot_pacf
-from statsmodels.tsa.seasonal import STL
+from statsmodels.tsa.seasonal import STL, seasonal_decompose
 from statsmodels.tsa.stattools import pacf
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.stats.diagnostic import acorr_ljungbox
+
+from scipy.stats import jarque_bera
+
+from pmdarima import auto_arima
+
 
 
 class TemporalController:
@@ -233,36 +242,38 @@ class TemporalController:
         diff_order: Optional[int] = None,
         num_lags: Optional[int] = None,
         alpha: Optional[int] = None,
+        serie: Optional[pd.Series] = None
     ):
-        if uf:
-            self.df = self.df[self.df["SIGLA_UF"] == uf]
+        if serie is None:
+            if uf:
+                self.df = self.df[self.df["SIGLA_UF"] == uf]
 
-        if syndrome:
-            self.df = self.df[self.df["CLASSI_FIN"] == syndrome]
+            if syndrome:
+                self.df = self.df[self.df["CLASSI_FIN"] == syndrome]
 
-        if year:
-            self.df = self.df[self.df["DT_NOTIFIC"].dt.year == year]
+            if year:
+                self.df = self.df[self.df["DT_NOTIFIC"].dt.year == year]
 
-        if evolution:
-            self.df = self.df[self.df["EVOLUCAO"] == evolution]
+            if evolution:
+                self.df = self.df[self.df["EVOLUCAO"] == evolution]
 
-        if granularity:
-            serie = self.df.groupby(
-                self.df["DT_NOTIFIC"].dt.to_period(f"{granularity}D")
-            ).size()
-        else:
-            serie = self.df.groupby(self.df["DT_NOTIFIC"].dt.to_period("W")).size()
+            if granularity:
+                serie = self.df.groupby(
+                    self.df["DT_NOTIFIC"].dt.to_period(f"{granularity}D")
+                ).size()
+            else:
+                serie = self.df.groupby(self.df["DT_NOTIFIC"].dt.to_period("W")).size()
 
-        serie.index = serie.index.to_timestamp()
+            serie.index = serie.index.to_timestamp()
 
-        # Remove Trend of the time series
-        serie = serie.diff().dropna()
+            # Remove Trend of the time series
+            serie = serie.diff().dropna()
 
-        # Remove Seasonality of the time series
-        if diff_order:
-            serie = serie.diff(diff_order).dropna()
-        else:
-            serie = serie.diff(7).dropna()
+            # Remove Seasonality of the time series
+            if diff_order:
+                serie = serie.diff(diff_order).dropna()
+            else:
+                serie = serie.diff(7).dropna()
 
         aux_num_lags = num_lags if num_lags else 25
         aux_alpha = alpha if alpha else 0.01
@@ -279,13 +290,107 @@ class TemporalController:
             "upperY": upper_y.tolist(),
         }
 
-    def serie_stl_decomposition(
+
+    def partial_correlogram(
+        self,
+        uf: Optional[str] = None,
+        syndrome: Optional[str] = None,
+        year: Optional[int] = None,
+        evolution: Optional[str] = None,
+        granularity: Optional[int] = None,
+        diff_order: Optional[int] = None,
+        num_lags: Optional[int] = None,
+        alpha: Optional[int] = None,
+        serie: Optional[pd.Series] = None
+    ):
+        if serie is None:
+            if uf:
+                self.df = self.df[self.df["SIGLA_UF"] == uf]
+
+            if syndrome:
+                self.df = self.df[self.df["CLASSI_FIN"] == syndrome]
+
+            if year:
+                self.df = self.df[self.df["DT_NOTIFIC"].dt.year == year]
+
+            if evolution:
+                self.df = self.df[self.df["EVOLUCAO"] == evolution]
+
+            if granularity:
+                serie = self.df.groupby(
+                    self.df["DT_NOTIFIC"].dt.to_period(f"{granularity}D")
+                ).size()
+            else:
+                serie = self.df.groupby(self.df["DT_NOTIFIC"].dt.to_period("W")).size()
+
+            serie.index = serie.index.to_timestamp()
+
+            serie = serie.diff().dropna()
+
+            if diff_order:
+                serie = serie.diff(diff_order).dropna()
+            else:
+                serie = serie.diff(7).dropna()
+
+        aux_num_lags = num_lags if num_lags else 25
+        aux_alpha = alpha if alpha else 0.01
+
+        corr_array = tsa.pacf(serie, nlags=aux_num_lags, alpha=aux_alpha)
+
+        lower_y = corr_array[1][:, 0] - corr_array[0]
+        upper_y = corr_array[1][:, 1] - corr_array[0]
+
+        return {
+            "autocorrelations": corr_array[0].tolist(),
+            "confidenceIntervals": corr_array[1].tolist(),
+            "lowerY": lower_y.tolist(),
+            "upperY": upper_y.tolist(),
+        }
+
+
+    def stationarity_test(
+        self,
+        uf: Optional[str] = None,
+        syndrome: Optional[str] = None,
+        year: Optional[int] = None,
+        evolution: Optional[str] = None,
+        significance_level: Optional[float] = 0.05,
+        serie: Optional[pd.Series] = None
+    ):  
+        if serie is None:
+            if uf:
+                self.df = self.df[self.df["SIGLA_UF"] == uf]
+
+            if syndrome:
+                self.df = self.df[self.df["CLASSI_FIN"] == syndrome]
+
+            if year:
+                self.df = self.df[self.df["DT_NOTIFIC"].dt.year == year]
+
+            if evolution:
+                self.df = self.df[self.df["EVOLUCAO"] == evolution]
+
+            serie = self.df.groupby("DT_NOTIFIC").size()
+
+        adf_result = adfuller(serie)
+        
+        return {
+            'testStatistic': adf_result[0],
+            'pValue': adf_result[1],
+            'criticalValues': adf_result[4],
+            'stationary': "True" if adf_result[1] < significance_level else "False"
+        }
+    
+
+    def get_stl_decomposition_data(
         self,
         uf: Optional[str] = None,
         syndrome: Optional[str] = None,
         year: Optional[int] = None,
         evolution: Optional[str] = None,
         seasonal: Optional[int] = None,
+        num_lags: Optional[int] = None, # Auto Correlogram and P.A.C plot
+        alpha: Optional[int] = None,
     ):
         if uf:
             self.df = self.df[self.df["SIGLA_UF"] == uf]
@@ -309,8 +414,189 @@ class TemporalController:
         serie["Seasonal_values"] = results.seasonal.values
         serie["Resid_values"] = results.resid.values
         serie["DT_NOTIFIC"] = serie["DT_NOTIFIC"].dt.strftime("%Y-%m-%d")
+        
+        stationarity_test_result = self.stationarity_test(serie=serie['Resid_values'])
+        correlogram_data = self.correlogram(serie=serie['Resid_values'], num_lags=num_lags, alpha=alpha)
+        partial_correlogram_data = self.partial_correlogram(serie=serie['Resid_values'], num_lags=num_lags, alpha=alpha)
 
-        return serie.to_dict(orient="list")
+        return {
+            "stlData":serie.to_dict(orient="list"),
+            "stationarityTest":stationarity_test_result,
+            "correlogram":correlogram_data,
+            "partialCorrelogram":partial_correlogram_data
+        }
+    
+
+    def get_seasonal_decomposition_data(
+        self,
+        uf: Optional[str] = None,
+        syndrome: Optional[str] = None,
+        year: Optional[int] = None,
+        evolution: Optional[str] = None,
+        period: Optional[int] = 5,  # Default to daily data if no frequency is specified
+        model: Optional[str] = 'additive',  # The type of decomposition ('additive' or 'multiplicative')
+        num_lags: Optional[int] = None, # Auto Correlogram and P.A.C plot
+        alpha: Optional[int] = None,
+    ):
+        if uf:
+            self.df = self.df[self.df["SIGLA_UF"] == uf]
+
+        if syndrome:
+            self.df = self.df[self.df["CLASSI_FIN"] == syndrome]
+
+        if year:
+            self.df = self.df[self.df["DT_NOTIFIC"].dt.year == year]
+
+        if evolution:
+            self.df = self.df[self.df["EVOLUCAO"] == evolution]
+
+        serie = self.df.groupby("DT_NOTIFIC").size()
+        
+        decomposition = seasonal_decompose(serie, model=model, period=period)
+
+        serie = serie.reset_index().rename(columns={0: "Count"})
+        serie["Trend_values"] = decomposition.trend.values
+        serie["Seasonal_values"] = decomposition.seasonal.values
+        serie["Resid_values"] = decomposition.resid.values
+        serie["DT_NOTIFIC"] = serie["DT_NOTIFIC"].dt.strftime("%Y-%m-%d")
+
+        serie = serie.dropna()
+
+        stationarity_test_result = self.stationarity_test(serie=serie['Resid_values'])
+        correlogram_data = self.correlogram(serie=serie['Resid_values'], num_lags=num_lags, alpha=alpha)
+        partial_correlogram_data = self.partial_correlogram(serie=serie['Resid_values'], num_lags=num_lags, alpha=alpha)
+
+        return {
+            "seasonalData":serie.to_dict(orient="list"),
+            "stationarityTest":stationarity_test_result,
+            "correlogram":correlogram_data,
+            "partialCorrelogram":partial_correlogram_data
+        }
+
+    
+    @staticmethod
+    def norm_test(residuos: pd.Series):
+        teste = jarque_bera(residuos)
+        return {
+            "Jarque-Bera": teste.pvalue,
+            "normResid": "True" if teste.pvalue > 0.05 else "False",
+        }
+    
+
+    @staticmethod
+    def independence_test(residuos: pd.Series, lags: int = 5):
+        df = acorr_ljungbox(residuos, lags=[lags], return_df=False)
+        return {
+            "Ljung-Box": df.iloc[0]['lb_pvalue'],
+            "independenceResid": "True" if df.iloc[0]['lb_pvalue'] > 0.05 else "False",
+        }
+
+
+    def get_predict_data(
+        self,
+        uf: Optional[str] = None,
+        syndrome: Optional[str] = None,
+        year: Optional[int] = None,
+        evolution: Optional[str] = None,
+        forecast_steps: Optional[int] = 30,
+        independence_lags: Optional[int] = 4,
+        num_lags_correlogram: Optional[int] = 10, # Auto Correlogram and P.A.C plot
+        alpha_correlogram: Optional[int] = 0.01,
+    ):
+        if uf:
+            self.df = self.df[self.df["SIGLA_UF"] == uf]
+
+        if syndrome:
+            self.df = self.df[self.df["CLASSI_FIN"] == syndrome]
+
+        if year:
+            self.df = self.df[self.df["DT_NOTIFIC"].dt.year == year]
+
+        if evolution:
+            self.df = self.df[self.df["EVOLUCAO"] == evolution]
+
+        serie = self.df.groupby("DT_NOTIFIC").size()
+        
+        """
+            Search Best Model
+        """
+        model_search = auto_arima(
+            serie,
+            seasonal=True,
+            trace=True, 
+            error_action='ignore', 
+            suppress_warnings=True
+        )
+
+        aic = model_search.aic()
+        order = model_search.order
+        seasonal_order = model_search.seasonal_order
+
+        """
+            Adjusted Serie
+        """
+        adjusted_serie = model_search.predict_in_sample()
+
+        fitted_model_resid = serie - adjusted_serie
+        norm_result = self.norm_test(fitted_model_resid)
+        independence_result = self.independence_test(fitted_model_resid, independence_lags)
+
+        adjusted_serie = adjusted_serie.reset_index().rename(columns={'index':'DT_NOTIFIC', 'predicted_mean':'Predict'})
+        # adjusted_serie["DT_NOTIFIC"] = adjusted_serie["DT_NOTIFIC"].dt.strftime("%Y-%m-%d")
+        
+        """
+            Model Paramns
+        """
+        p, d, q = model_search.order
+        P, D, Q, m = model_search.seasonal_order
+
+        """
+            Adjusted Model and Get Infos
+        """
+        model = SARIMAX(serie, order=(p, d, q), seasonal_order=(P, D, Q, m))
+        results = model.fit(disp=False, maxiter=500)
+        forecast = results.get_forecast(steps=forecast_steps)
+
+        pred_mean = forecast.predicted_mean
+        pred_mean = pred_mean.reset_index().rename(columns={'index':'DT_NOTIFIC'})
+        pred_mean["DT_NOTIFIC"] = pred_mean["DT_NOTIFIC"].dt.strftime("%Y-%m-%d")
+
+        pred_conf = forecast.conf_int(alpha=0.05)
+        pred_conf = pred_conf.reset_index().rename(columns={'index':'DT_NOTIFIC'})
+        pred_conf["DT_NOTIFIC"] = pred_conf["DT_NOTIFIC"].dt.strftime("%Y-%m-%d")
+
+        pred_resid = results.resid.reset_index().rename(columns={'index':'DT_NOTIFIC', 0:'Resid'})
+        pred_resid["DT_NOTIFIC"] = pred_resid["DT_NOTIFIC"].dt.strftime("%Y-%m-%d")
+
+        pred_resid_correlogram = self.correlogram(
+            serie=pred_resid['Resid'], num_lags=num_lags_correlogram, alpha=alpha_correlogram
+        )
+        pred_resid_partial_correlogram = self.partial_correlogram(
+            serie=pred_resid['Resid'], num_lags=num_lags_correlogram, alpha=alpha_correlogram
+        )
+
+
+        serie = serie.reset_index()
+        serie = serie.rename(columns={0: "Count"})
+        serie["DT_NOTIFIC"] = serie["DT_NOTIFIC"].dt.strftime("%Y-%m-%d")
+        serie["Predict"] = adjusted_serie["Predict"]
+
+        return {
+            "aic": aic, # Card
+            "order": order, # Card
+            "seasonalOrder": seasonal_order, # Card
+            "summary": model_search.summary().as_text(), # Text base64
+            "originalSerie":serie.to_dict(orient="list"),   # Serie
+            # "adjustedSerie":adjusted_serie.to_dict(orient="list"),   # Serie
+            "normTest":norm_result, # Card
+            "independenceTest":independence_result, # Card
+            "predictResid":pred_resid.to_dict(orient="list"),  # Serie
+            "predictMean":pred_mean.to_dict(orient="list"), # Serie
+            "predictConf":pred_conf.to_dict(orient="list"),  # Serie
+            "predictCorrelogram":pred_resid_correlogram,    # Corr
+            "predictPartialCorrelogram":pred_resid_partial_correlogram  # Corr
+        }
+    
 
     def serie_lag_plot(
         self,
@@ -348,6 +634,7 @@ class TemporalController:
             "minVal": min_val,
             "lag": lag,
         }
+
 
     def get_overview_data(
         self,
@@ -413,6 +700,7 @@ class TemporalController:
             "serieLagPlot": self.serie_lag_plot(uf, syndrome, year, evolution),
         }
 
+
     def get_serie_lag_plot(
         self,
         uf: Optional[str] = None,
@@ -427,3 +715,4 @@ class TemporalController:
             )
             for i in range(4)
         }
+    
